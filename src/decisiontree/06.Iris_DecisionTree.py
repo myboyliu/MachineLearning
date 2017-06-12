390637bfa6b78602c78b5b41ca57bcacb0bb7c3b
JJZHK <jeffcobile@gmail.com>
2017-06-12 17:13:36 +0800

    决策树代码

--- src/decisiontree/06.Iris_DecisionTree.py
+++ src/decisiontree/06.Iris_DecisionTree.py
@@ -11,10 +11,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 import pydotplus
 
+
 # 花萼长度、花萼宽度，花瓣长度，花瓣宽度
 iris_feature_E = 'sepal length', 'sepal width', 'petal length', 'petal width'
 iris_feature = u'花萼长度', u'花萼宽度', u'花瓣长度', u'花瓣宽度'
 iris_class = 'Iris-setosa', 'Iris-versicolor', 'Iris-virginica'
+
 
 if __name__ == "__main__":
     mpl.rcParams['font.sans-serif'] = [u'SimHei']
@@ -22,25 +24,27 @@ if __name__ == "__main__":
 
     path = 'iris.data'  # 数据文件路径
     data = pd.read_csv(path, header=None)
-
-    x = data.values[:,:2]
-    y = data.values[:, -1:]
+    x = data[np.arange(4)]
+    y = pd.Categorical(data[4]).codes
     # 为了可视化，仅使用前两列特征
-    # x = x[:, :2]
+    x = x.iloc[:, :2]
     x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.7, random_state=1)
+    print(y_test.shape)
 
     # 决策树参数估计
     # min_samples_split = 10：如果该结点包含的样本数目大于10，则(有可能)对其分支
     # min_samples_leaf = 10：若将某结点分支后，得到的每个子结点样本数目都大于10，则完成分支；否则，不进行分支
-    model = DecisionTreeClassifier(criterion='entropy') #criterion = 'gini' or 'entropy'
+    model = DecisionTreeClassifier(criterion='entropy')
     model.fit(x_train, y_train)
     y_test_hat = model.predict(x_test)      # 测试数据
 
     # 保存
+    # dot -Tpng my.dot -o my.png
     # 1、输出
     with open('iris.dot', 'w') as f:
         tree.export_graphviz(model, out_file=f)
-
+    # 2、给定文件名
+    # tree.export_graphviz(model, out_file='iris1.dot')
     # 3、输出为pdf格式
     dot_data = tree.export_graphviz(model, out_file=None, feature_names=iris_feature_E, class_names=iris_class,
                                     filled=True, rounded=True, special_characters=True)
@@ -49,26 +53,34 @@ if __name__ == "__main__":
     f = open('iris.png', 'wb')
     f.write(graph.create_png())
     f.close()
+
     # 画图
     N, M = 50, 50  # 横纵各采样多少个值
-    x1_min = x.min()
-    x2_min = x.min()
-    x1_max = x.max()
-    x2_max = x.max()
+    x1_min, x2_min = x.min()
+    x1_max, x2_max = x.max()
     t1 = np.linspace(x1_min, x1_max, N)
     t2 = np.linspace(x2_min, x2_max, M)
     x1, x2 = np.meshgrid(t1, t2)  # 生成网格采样点
     x_show = np.stack((x1.flat, x2.flat), axis=1)  # 测试点
+    print(x_show.shape)
+
+    # # 无意义，只是为了凑另外两个维度
+    # # 打开该注释前，确保注释掉x = x[:, :2]
+    # x3 = np.ones(x1.size) * np.average(x[:, 2])
+    # x4 = np.ones(x1.size) * np.average(x[:, 3])
+    # x_test = np.stack((x1.flat, x2.flat, x3, x4), axis=1)  # 测试点
 
     cm_light = mpl.colors.ListedColormap(['#A0FFA0', '#FFA0A0', '#A0A0FF'])
     cm_dark = mpl.colors.ListedColormap(['g', 'r', 'b'])
     y_show_hat = model.predict(x_show)  # 预测值
-    y_show_hat = y_show_hat.reshape(x1.shape)  # 使之与输入的形状相同
-    plt.figure(facecolor='w')
+    print(y_show_hat.shape)
     print(y_show_hat)
+    y_show_hat = y_show_hat.reshape(x1.shape)  # 使之与输入的形状相同
+    print(y_show_hat)
+    plt.figure(facecolor='w')
     plt.pcolormesh(x1, x2, y_show_hat, cmap=cm_light)  # 预测值的显示
-    plt.scatter(x_test[0], x_test[1], c='#FFFFFF', edgecolors='k', s=150, zorder=10, cmap=cm_dark, marker='*')  # 测试数据
-    plt.scatter(x[0], x[1], c='#00FFEE', edgecolors='k', s=40, cmap=cm_dark)  # 全部数据
+    plt.scatter(x_test[0], x_test[1], c=y_test.ravel(), edgecolors='k', s=150, zorder=10, cmap=cm_dark, marker='*')  # 测试数据
+    plt.scatter(x[0], x[1], c=y.ravel(), edgecolors='k', s=40, cmap=cm_dark)  # 全部数据
     plt.xlabel(iris_feature[0], fontsize=15)
     plt.ylabel(iris_feature[1], fontsize=15)
     plt.xlim(x1_min, x1_max)
