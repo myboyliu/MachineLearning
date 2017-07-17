f82bb32aed905047822acf616fb12bdc98acd313
JJZHK <jeffcobile@gmail.com>
2017-07-18 05:22:39 +0800

    滤波器

--- Hello.py
+++ Hello.py
@@ -1,45 +1,43 @@
 import cv2
+import matplotlib as mpl
 import numpy as np
 from matplotlib import pyplot as plt
-img = cv2.imread('images/12/opencv-logo.png')
+image_Ori = cv2.imread('images/12/Lena.png')
+image_Dirty = cv2.imread('images/12/Dirty.png')
+mask = cv2.imread('images/12/Lena_Dirty.png',0)
 
-plt.figure(figsize=(13,7))
-plt.subplot(231)
-plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-plt.title('Original')
-plt.xticks([]), plt.yticks([])
+mpl.rcParams['font.sans-serif'] = ['FangSong'] # 指定默认字体
+mpl.rcParams['axes.unicode_minus'] = False # 解决保存图像是负号'-'显示为方块的问题
 
-kernel = np.ones((3,3),np.float32)/9
-dst = cv2.filter2D(img,-1,kernel)
-plt.subplot(232),
-plt.imshow(cv2.cvtColor(dst, cv2.COLOR_BGR2RGB))
-plt.title('3*3')
-plt.xticks([]), plt.yticks([])
+def transfer(img):
+    height = img.shape[0]
+    width = img.shape[1]
+    rects = ((width - 169, height - 22, width, height),(1, 1, 164, 21)) #水印区域
 
-kernel = np.ones((5,5),np.float32)/25
-dst = cv2.filter2D(img,-1,kernel)
-plt.subplot(233)
-plt.imshow(cv2.cvtColor(dst, cv2.COLOR_BGR2RGB))
-plt.title('5*5')
-plt.xticks([]), plt.yticks([])
+    mask = np.zeros(img.shape[:2], np.uint8)
+    for rect in rects:
+        x1, y1, x2, y2 = rect
+        cv2.rectangle(mask, (x1, y1), (x2, y2), (255, 255, 255), -1)
+        img = cv2.inpaint(img, mask, 10.0, cv2.INPAINT_TELEA) #蒙版
+    return img
 
-kernel = np.ones((10,10),np.float32)/100
-dst = cv2.filter2D(img,-1,kernel)
-plt.subplot(234)
-plt.imshow(cv2.cvtColor(dst, cv2.COLOR_BGR2RGB))
-plt.title('9*9')
-plt.xticks([]), plt.yticks([])
-
-kernel = np.ones((20,20),np.float32)/300
-dst = cv2.filter2D(img,-1,kernel)
-plt.subplot(235)
-plt.imshow(cv2.cvtColor(dst, cv2.COLOR_BGR2RGB))
-plt.title('19*19')
-plt.xticks([]), plt.yticks([])
-
-blur = cv2.blur(img,(3,3)) # blur就是做的平滑均值滤波
-plt.subplot(236)
-plt.imshow(cv2.cvtColor(blur, cv2.COLOR_BGR2RGB))
-plt.title('3*3')
-plt.xticks([]), plt.yticks([])
+# plt.figure(figsize=(10,5), facecolor='w')
+# plt.subplot(141)
+# plt.imshow(cv2.cvtColor(image_Ori, cv2.COLOR_BGR2RGB))
+# plt.title('原始图片')
+# plt.xticks([]), plt.yticks([])
+#
+# plt.subplot(142)
+# plt.imshow(cv2.cvtColor(image_Dirty, cv2.COLOR_BGR2RGB))
+# plt.title('污点图片')
+# plt.xticks([]), plt.yticks([])
+#
+# plt.subplot(143)
+# img = transfer(img)
+# plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+# plt.title('修复图片')
+# plt.xticks([]), plt.yticks([])
+image_Dirty = transfer(image_Dirty)
+plt.imshow(image_Dirty)
+plt.suptitle('低通滤波器比较')
 plt.show()
