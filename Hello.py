44c4c58ef4090708003bca2884a323a2c035b8e3
Jeff K Zhang <jeffcobile@gmail.com>
2017-07-20 15:19:29 +0800

    计算机视觉

--- Hello.py
+++ Hello.py
@@ -1,61 +1,70 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from skimage import filters,feature
+import matplotlib as mpl
 
-def imConv(image_array,suanzi, kernelDim):
-    image = image_array.copy()
+# 打开图像并转化成灰度图像
+mpl.rcParams['font.sans-serif'] = ['FangSong'] # 指定默认字体
+mpl.rcParams['axes.unicode_minus'] = False # 解决保存图像是负号'-'显示为方块的问题
 
-    dim1,dim2 = image.shape
-    newImageDelta = int((kernelDim - 1)/2)
-    for i in range(newImageDelta,dim1-newImageDelta):
-        for j in range(newImageDelta,dim2-newImageDelta):
-            image[i,j] = (image_array[(i-newImageDelta):(i+newImageDelta+1),(j-newImageDelta):(j+newImageDelta+1)]*suanzi).sum()
+plt.figure(figsize=(17,13), facecolor='w')
 
-    image = image*(255.0/image.max())
+image = cv2.imread('images/12/Lena.png', cv2.IMREAD_GRAYSCALE)
+image = cv2.GaussianBlur(image
+                         ,(3,3),0)
+row = 3
+col = 4
+plt.subplot(row,col,1)
+plt.imshow(image,cmap=plt.cm.gray)
+plt.title(u'原图')
 
-    return image
-image_BGR = cv2.imread('images/12/Lena.png')
-image_RGB = cv2.cvtColor(image_BGR, cv2.COLOR_BGR2RGB)
-image_Gray = cv2.cvtColor(image_RGB, cv2.COLOR_RGB2GRAY)
+edges = filters.prewitt(image)
+plt.subplot(row,col,2)
+plt.imshow(edges,cmap=plt.cm.gray)
+plt.title(u'Prewitt算子')
 
-kernelDim = 3
-kernel = np.ones((kernelDim,kernelDim), np.float32) / (kernelDim * kernelDim)
-suanzi_x = np.array([[-1, 0, 1],
-                     [ -1, 0, 1],
-                     [ -1, 0, 1]])
-#
-# y方向的Prewitt算子
-suanzi_y = np.array([[-1,-1,-1],
-                     [ 0, 0, 0],
-                     [ 1, 1, 1]])
+edges = filters.roberts(image)
+plt.subplot(row,col,3)
+plt.imshow(edges,cmap=plt.cm.gray)
+plt.title(u'Roberts算子')
 
-RuiHua = np.array([[0,-1,0],
-                   [ -1, 3, -1],
-                   [0, -1, 0]])
-plt.figure(figsize=(15,8))
-plt.subplot(231)
-plt.imshow(image_Gray, cmap='gray')
+edges = filters.sobel(image)
+plt.subplot(row,col,4)
+plt.imshow(edges,cmap=plt.cm.gray)
+plt.title(u'Sobel算子')
 
-plt.subplot(232)
-image_x = imConv(image_Gray, suanzi_x,kernelDim)
-plt.imshow(image_x, cmap='gray')
+edges = cv2.Sobel(image,cv2.CV_64F,1,0,ksize=5)
+plt.subplot(row,col,5)
+plt.imshow(edges,cmap=plt.cm.gray)
+plt.title(u'Sobel-X算子')
 
-plt.subplot(233)
-image_y = imConv(image_Gray,suanzi_y, kernelDim)
-plt.imshow(image_y, cmap='gray')
+edges = cv2.Sobel(image,cv2.CV_64F,0,1,ksize=5)
+plt.subplot(row,col,6)
+plt.imshow(edges,cmap=plt.cm.gray)
+plt.title(u'Sobel-Y算子')
 
-plt.subplot(234)
-# 得到梯度矩阵
-image_xy = np.sqrt(image_x**2+image_y**2)
-# 梯度矩阵统一到0-255
-image_xy = (255.0/image_xy.max())*image_xy
-plt.imshow(image_xy, cmap='gray')
+edges = cv2.Laplacian(image,cv2.CV_64F)
+plt.subplot(row,col,7)
+plt.imshow(edges,cmap=plt.cm.gray)
+plt.title(u'Laplacian算子')
 
-plt.subplot(235)
-plt.imshow(imConv(image_Gray, kernel, kernelDim), cmap='gray')
+edges = feature.canny(image)
+plt.subplot(row,col,8)
+plt.imshow(edges,cmap=plt.cm.gray)
+plt.title(u'canny算子 $\sigma$=1')
 
-plt.subplot(236)
-plt.imshow(imConv(image_Gray, RuiHua, kernelDim), cmap='gray')
+edges = feature.canny(image, sigma=3)
+plt.subplot(row,col,9)
+plt.imshow(edges,cmap=plt.cm.gray)
+plt.title(u'canny算子 $\sigma$=3')
+
+i1 = cv2.GaussianBlur(image, (15,15), 5)
+i2 = cv2.GaussianBlur(image, (21,21), 5)
+GaussSub = cv2.subtract(i1, i2)
+plt.subplot(row,col,10)
+plt.imshow(GaussSub,cmap=plt.cm.gray)
+plt.title(u'高斯差分')
+
+plt.suptitle(u'边缘算子比较', fontsize=18)
 plt.show()
-
-
