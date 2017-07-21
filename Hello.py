a9f86d4b2e75ed1854825ed93ddf91eca9df2276
JJZHK <jeffcobile@gmail.com>
2017-07-21 23:01:18 +0800

    计算机视觉-图像特征

--- Hello.py
+++ Hello.py
@@ -1,70 +1,85 @@
-import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-from skimage import filters,feature
 import matplotlib as mpl
+import cv2
+from skimage import filters
+import scipy.signal as ss
 
-# 打开图像并转化成灰度图像
 mpl.rcParams['font.sans-serif'] = ['FangSong'] # 指定默认字体
 mpl.rcParams['axes.unicode_minus'] = False # 解决保存图像是负号'-'显示为方块的问题
 
-plt.figure(figsize=(17,13), facecolor='w')
+image = cv2.imread('images/12/USA.png')
+image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-image = cv2.imread('images/12/Lena.png', cv2.IMREAD_GRAYSCALE)
-image = cv2.GaussianBlur(image
-                         ,(3,3),0)
-row = 3
-col = 4
-plt.subplot(row,col,1)
-plt.imshow(image,cmap=plt.cm.gray)
-plt.title(u'原图')
+plt.figure(figsize=(25,5), facecolor='w')
+plt.subplot(241)
+plt.title('原图')
+plt.imshow(image_gray, cmap='gray')
 
-edges = filters.prewitt(image)
-plt.subplot(row,col,2)
-plt.imshow(edges,cmap=plt.cm.gray)
-plt.title(u'Prewitt算子')
+plt.subplot(242)
+plt.title('Sobel-X')
+SobelX = np.array([[-1, 0, 1],
+                   [ -2, 0, 2],
+                   [ -1, 0, 1]])
+image_x = ss.convolve2d(image_gray, SobelX, mode="same")
+plt.imshow(image_x, cmap='gray')
 
-edges = filters.roberts(image)
-plt.subplot(row,col,3)
-plt.imshow(edges,cmap=plt.cm.gray)
-plt.title(u'Roberts算子')
+plt.subplot(243)
+plt.title('Sobel-Y')
+SobelY = np.array([[-1,-2,-1],
+                   [ 0, 0, 0],
+                   [ 1, 2, 1]])
+image_y = ss.convolve2d(image_gray, SobelY, mode="same")
+plt.imshow(image_y, cmap='gray')
 
-edges = filters.sobel(image)
-plt.subplot(row,col,4)
-plt.imshow(edges,cmap=plt.cm.gray)
-plt.title(u'Sobel算子')
+grident = image_x* image_x+ image_y * image_y
+gridentSqrt = np.sqrt(grident)
+b = (255/gridentSqrt.max()) * gridentSqrt
 
-edges = cv2.Sobel(image,cv2.CV_64F,1,0,ksize=5)
-plt.subplot(row,col,5)
-plt.imshow(edges,cmap=plt.cm.gray)
-plt.title(u'Sobel-X算子')
+plt.subplot(244)
+plt.title('Sobel')
+image_Sobel = b.copy()
+plt.imshow(image_Sobel, cmap='gray')
 
-edges = cv2.Sobel(image,cv2.CV_64F,0,1,ksize=5)
-plt.subplot(row,col,6)
-plt.imshow(edges,cmap=plt.cm.gray)
-plt.title(u'Sobel-Y算子')
+scale = 4;
+cuteoff = scale*np.mean(b)
 
-edges = cv2.Laplacian(image,cv2.CV_64F)
-plt.subplot(row,col,7)
-plt.imshow(edges,cmap=plt.cm.gray)
-plt.title(u'Laplacian算子')
+plt.subplot(245)
+plt.title('Sobel二值化')
 
-edges = feature.canny(image)
-plt.subplot(row,col,8)
-plt.imshow(edges,cmap=plt.cm.gray)
-plt.title(u'canny算子 $\sigma$=1')
+b[b>=cuteoff] = 255
+b[b<cuteoff] = 0
+plt.imshow(b, cmap='gray')
 
-edges = feature.canny(image, sigma=3)
-plt.subplot(row,col,9)
-plt.imshow(edges,cmap=plt.cm.gray)
-plt.title(u'canny算子 $\sigma$=3')
+rThresh = scale * np.mean(grident)
+image_NMS = b.copy()
+for i in range(1, image_gray.shape[0]-1):
+    sbx = image_x[i]
+    sby = image_y[i]
+    pre = b[i-1]
+    cur = b[i]
+    lst = b[i+1]
+    for j in range(1, image_gray.shape[1]-1):
+        if(cur[j] > cuteoff and (
+                (sbx[j] > sby[j] and cur[j] > cur[j-1] and cur[j] > cur[j+1]) or
+                (sby[j] > sbx[j] and cur[j] > pre[j] and cur[j] > lst[j])
+        )):
+            image_NMS[i,j]=0
 
-i1 = cv2.GaussianBlur(image, (15,15), 5)
-i2 = cv2.GaussianBlur(image, (21,21), 5)
-GaussSub = cv2.subtract(i1, i2)
-plt.subplot(row,col,10)
-plt.imshow(GaussSub,cmap=plt.cm.gray)
-plt.title(u'高斯差分')
+plt.subplot(246)
+plt.title('Sobel非极大值抑制1')
+plt.imshow(image_NMS, cmap='gray')
 
-plt.suptitle(u'边缘算子比较', fontsize=18)
+image_NMS = b.copy()
+for i in range(1, image_gray.shape[0]-1):
+    for j in range(1, image_gray.shape[1]-1):
+        if (grident[i,j] > rThresh and
+                ((grident[i,j] > grident[i-1,j] and grident[i,j] > grident[i+1, j])
+                 or
+                     (grident[i,j] > grident[i, j-1] and grident[i,j] > grident[i, j+1]))):
+            image_NMS[i,j]=1
+
+plt.subplot(247)
+plt.title('Sobel非极大值抑制2')
+plt.imshow(image_NMS, cmap='gray')
 plt.show()
