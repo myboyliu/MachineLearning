94d38d84f47ccfdcac5015c16339f329881aa8fb
JJZHK <jeffcobile@gmail.com>
2017-08-14 14:35:32 +0800

    Tensorflow

--- 01.Knowledge-13Tensorflow实例/02.代码实现-01机器学习-07传统神经网络.py
+++ 01.Knowledge-13Tensorflow实例/02.代码实现-01机器学习-07传统神经网络.py
@@ -40,7 +40,7 @@ batch_size = 100
 n_batch = mnist.train.num_examples // batch_size
 
 x = tf.placeholder(tf.float32, [None, 784])
-y = tf.placeholder(tf.float32, [None, 10])
+y = tf.placeholder(tf.float32, [None, 10]) # 最后的输出是0~9这10个数字，所以是10
 keep_prob = tf.placeholder(tf.float32)
 
 W1 = tf.Variable(tf.truncated_normal([784, 300], stddev=0.1)) # 不要初始化为0
@@ -48,22 +48,20 @@ b1 = tf.Variable(tf.zeros([300]) + 0.1)
 L1 = tf.nn.tanh(tf.matmul(x, W1) + b1)
 L1_drop = tf.nn.dropout(L1, keep_prob=keep_prob)
 
-W2 = tf.Variable(tf.truncated_normal([300, 300], stddev=0.1)) # 不要初始化为0
-b2 = tf.Variable(tf.zeros([300]) + 0.1)
+W2 = tf.Variable(tf.truncated_normal([300, 150], stddev=0.1)) # 不要初始化为0
+b2 = tf.Variable(tf.zeros([150]) + 0.1)
 L2 = tf.nn.tanh(tf.matmul(L1_drop, W2) + b2)
 L2_drop = tf.nn.dropout(L2, keep_prob=keep_prob)
 
-W3 = tf.Variable(tf.truncated_normal([300, 100], stddev=0.1)) # 不要初始化为0
-b3 = tf.Variable(tf.zeros([100]) + 0.1)
+W3 = tf.Variable(tf.truncated_normal([150, 50], stddev=0.1)) # 不要初始化为0
+b3 = tf.Variable(tf.zeros([50]) + 0.1)
 L3 = tf.nn.tanh(tf.matmul(L2_drop, W3) + b3)
 L3_drop = tf.nn.dropout(L3, keep_prob=keep_prob)
 
-W4 = tf.Variable(tf.truncated_normal([100, 10], stddev=0.1)) # 不要初始化为0
+W4 = tf.Variable(tf.truncated_normal([50, 10], stddev=0.1)) # 不要初始化为0
 b4 = tf.Variable(tf.zeros([10]) + 0.1)
 
 prediction = tf.nn.softmax(tf.matmul(L3_drop, W4) + b4)
-
-# loss = tf.reduce_mean(tf.square(y - prediction))
 
 loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y,
                                                               logits=prediction)) #交叉熵明显收敛更快，而且运行速度也快
